db245b91551285d6f342789b261517f56ac8aa38
robertrinh <57951795+robertrinh@users.noreply.github.com>
2023-02-22 20:16:55 +0100

    modified main for actual working bonus

--- main.c
+++ main.c
@@ -6,73 +6,74 @@
 /*   By: qtrinh <qtrinh@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/02 19:37:53 by qtrinh        #+#    #+#                 */
-/*   Updated: 2023/02/22 16:18:06 by qtrinh        ########   odam.nl         */
+/*   Updated: 2023/02/22 20:05:28 by qtrinh        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 #include <stdio.h>
 
-/* single fd */
-int	main(void)
+void	leak_check(void)
 {
-	int		fd;
-	char	*line;
-
-	fd = open("text.txt", O_RDONLY);
-	// fd = -1;
-	while (1)
-	{
-		line = get_next_line(fd);
-		printf("%s", line);
-		if (line == NULL)
-			break ;
-		free (line);
-	}
-	free (line);
-	close (fd);
-	// system("leaks a.out");
-	return (0);
+	system("leaks a.out");
 }
+//calls right after exit for leaks
 
-/* multiple fd */
+//CALLOC USE
+
+/* single fd */
 // int	main(void)
 // {
 // 	int		fd;
-// 	int		fd1;
-// 	int		fd2;
 // 	char	*line;
 
 // 	fd = open("text.txt", O_RDONLY);
-// 	fd1 = open("text2.txt", O_RDONLY);
-// 	fd2 = open("text3.txt", O_RDONLY);
-// 	line = "\0";
-// 	while (line)
+// 	// fd = -1;
+// 	while (1)
 // 	{
 // 		line = get_next_line(fd);
-// 		printf("%s", line);
 // 		if (line == NULL)
-// 			free(line);
-// 	}
-// 	line = "\0";
-// 	while (line)
-// 	{
-// 		line = get_next_line(fd1);
+// 			break ;
 // 		printf("%s", line);
-// 		if (line == NULL)
-// 			free(line);
-// 	}
-// 	line = "\0";
-// 	while (line)
-// 	{
-// 		line = get_next_line(fd2);
-// 		printf("%s", line);
-// 		if (line == NULL)
-// 			free(line);
+// 		free(line);
 // 	}
 // 	free(line);
 // 	close(fd);
-// 	close(fd1);
-// 	close(fd2);
+// 	atexit(&leak_check);
 // 	return (0);
 // }
+
+/* multiple fd */
+int	main(void)
+{
+	int		fd;
+	int		fd1;
+	int		fd2;
+	char	*line;
+
+	fd = open("text.txt", O_RDONLY);
+	fd1 = open("text2.txt", O_RDONLY);
+	fd2 = open("text3.txt", O_RDONLY);
+	line = "\0";
+	while (line)
+	{
+		line = get_next_line(fd);
+		if (line)
+			free(line);
+		printf("%s \n", line);
+		line = get_next_line(fd1);
+		if (line)
+			free(line);
+		printf("%s \n", line);
+		line = get_next_line(fd2);
+		if (line)
+			free(line);
+		printf("%s \n", line);
+	}
+	free(line);
+	close(fd);
+	close(fd1);
+	close(fd2);
+	atexit(&leak_check);
+	return (0);
+}
