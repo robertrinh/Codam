dd67b035c106370bb1d25b83ac5a580c192939b1
Quoc Quan Robert Trinh <qtrinh@f0r3s22.codam.nl>
2023-10-27 13:27:26 +0200

    fixed atod

--- string/ft_atod.c
+++ string/ft_atod.c
@@ -6,7 +6,7 @@
 /*   By: qtrinh <qtrinh@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/26 15:21:43 by qtrinh        #+#    #+#                 */
-/*   Updated: 2023/10/26 15:52:18 by qtrinh        ########   odam.nl         */
+/*   Updated: 2023/10/27 13:27:01 by qtrinh        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static void	get_decimal(char *str, double *decimal)
 	actual_decimal = 1;
 	if (*str == '.')
 		str++;
-	while (*str)
+	while (ft_isdigit(*str))
 	{
 		*decimal = *decimal * 10 + (*str - '0');
 		actual_decimal *= 10;
@@ -33,17 +33,19 @@ double	ft_atod(char *str)
 	int		sign;
 	double	decimal;
 	double	result;
+	int		i;
 
-	result = 0;
-	decimal = 0;
+	result = 0.0;
+	decimal = 0.0;
 	sign = 1;
+	i = 0;
 	if (*str == '-' || *str == '+')
 	{
 		if (*str == '-')
 			sign = -1;
 		str++;
 	}
-	while (*str && *str != '.')
+	while (ft_isdigit(*str))
 	{
 		result = result * 10 + (*str - '0');
 		str++;
@@ -51,9 +53,10 @@ double	ft_atod(char *str)
 	get_decimal(str, &decimal);
 	return (sign * (result + decimal));
 }
+
 // #include <stdio.h>
 // int	main(void)
 // {
-// 	printf("does it work: %.2f\n", ft_atod("-1.23"));
+// 	printf("does it work: %.2f\n", ft_atod("-1.#13"));
 // 	return (0);
 // }
