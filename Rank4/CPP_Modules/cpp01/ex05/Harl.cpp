ffc1f3dd5492debd563db68d72f77b1fbf3a1ca1
Quoc Quan Robert Trinh <qtrinh@f1r5s7.codam.nl>
2025-02-24 13:30:55 +0100

    deleted level status output msgs

--- Rank4/CPP_Modules/cpp01/ex05/Harl.cpp
+++ Rank4/CPP_Modules/cpp01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: robertrinh <robertrinh@student.codam.nl      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/02/21 16:30:59 by robertrinh    #+#    #+#                 */
-/*   Updated: 2025/02/21 17:43:07 by robertrinh    ########   odam.nl         */
+/*   Updated: 2025/02/24 13:29:37 by qtrinh        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,21 @@ Harl::~Harl()
 
 void Harl::_debug()
 {
-	std::cout << "[DEBUG]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << std::endl;
 }
 
 void Harl::_info()
 {
-	std::cout << "[INFO]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::_warning()
 {
-	std::cout << "[WARNING]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month" << std::endl;
 }
 
 void Harl::_error()
 {
-	std::cout << "[ERROR]" << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
@@ -55,6 +51,5 @@ void Harl::complain(std::string level)
 			return ;
 		}
 	}
-	std::cout << "[UNKNOWN]" << std::endl;
 	std::cout << "Harl does not comprehend what you are doing." << std::endl;
 }
