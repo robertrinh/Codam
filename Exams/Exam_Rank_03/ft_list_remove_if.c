b3be6aa2838659e14a8337db39440a27a692f4af
robertrinh <robert.trinh@yahoo.com>
2025-02-27 17:16:29 +0100

    added assignment descriptions

--- Exams/Exam_Rank_03/ft_list_remove_if.c
+++ Exams/Exam_Rank_03/ft_list_remove_if.c
@@ -1,5 +1,28 @@
+// Assignment name  : ft_list_remove_if
+// Expected files   : ft_list_remove_if.c
+// Allowed functions: free
+// --------------------------------------------------------------------------------
+
+// Write a function called ft_list_remove_if that removes from the
+// passed list any element the data of which is "equal" to the reference data.
+
+// It will be declared as follows :
+
+// void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)());
+
+// cmp takes two void* and returns 0 when both parameters are equal.
+
+// You have to use the ft_list.h file, which will contain:
+
+// $>cat ft_list.h
+// typedef struct      s_list
+// {
+//     struct s_list   *next;
+//     void            *data;
+// }                   t_list;
+// $>
+
 #include <stdlib.h>
-#include <string.h>
 // #include "ft_list.h" see the struct needed below
 
 typedef struct t_list
@@ -35,6 +58,7 @@ void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(void *, v
 }
 
 #include <stdio.h>
+#include <string.h>
 t_list *create_elem(void *data)
 {
     t_list *new = malloc(sizeof(t_list));
