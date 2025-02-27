97c5ed0194fb6834b21716efe442d886c1924595
robertrinh <robert.trinh@yahoo.com>
2025-02-27 17:12:33 +0100

    added sort_int_tab.c

---
+++ Exams/Exam_Rank_03/sort_int_tab.c
@@ -0,0 +1,49 @@
+void sort_int_tab(int *tab, unsigned int size)
+{
+	unsigned int i = 0;
+	int temp = 0;
+	
+	if (size < 2)
+	return ;
+	while (i < size - 1) // -1 because last pass would be redundant
+	{
+		unsigned int j = 0;
+		while (j < size - 1 - i) // -i to avoid rechecking sorted elements
+		{
+			if (tab[j] > tab[j + 1]) //bubble sort
+			{
+				temp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+//TESTER
+#include <stdio.h>
+int main(void)
+{
+	int arr[] = {5, 3, 8, -1, 2, 7, 4, 6};
+	unsigned int size = 8, i = 0;
+
+	printf("Before sorting: ");
+	while (i < size)
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
+	printf("\n");
+	sort_int_tab(arr, size);
+	printf("After sorting:  ");
+	i = 0;
+	while (i < size)
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
+	printf("\n");
+	return 0;
+}
