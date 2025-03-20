72ea8768b839b49f3a3f6b94e57272b638bd6d25
Quoc Quan Robert Trinh <qtrinh@f1r2s7.codam.nl>
2025-03-20 13:21:46 +0100

    modified test ex02

--- Rank4/CPP_Modules/cpp03/ex02/main.cpp
+++ Rank4/CPP_Modules/cpp03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: robertrinh <robertrinh@student.codam.nl      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/03/06 13:53:32 by robertrinh    #+#    #+#                 */
-/*   Updated: 2025/03/06 14:05:31 by robertrinh    ########   odam.nl         */
+/*   Updated: 2025/03/20 13:20:12 by qtrinh        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,25 @@
 
 int main(void)
 {
-	FragTrap one;
-	FragTrap two("Bob");
-	FragTrap three("Bab");
+	FragTrap cuz;
+	FragTrap fam("Blitzcrank");
+	FragTrap lad("Wasteman");
+	
+	std::cout << "Case checks below:" << std::endl;
+	std::cout << std::endl << "\033[1;34m>>Normal banter scenarios<<\033[0m" << std::endl;
+	cuz.attack("Emily");
+	cuz.takeDamage(5);
+	cuz.beRepaired(5);
+	cuz.highFivesGuys();
+	
+	std::cout << std::endl << "\033[1;34m>>When a brother is getting killed<<\033[0m" << std::endl;
+	fam.takeDamage(50);
+	fam.takeDamage(50);
+	fam.takeDamage(1);
 
-	two.attack("Bab");
+	std::cout << std::endl << "\033[1;34m>>Out of energy? SPAMMING REPAIR (but no ClapTrap energy tiers)<<\033[0m" << std::endl;
+	for (int i = 0; i < 11; i++)
+		lad.beRepaired(8);
 
-	two.highFivesGuys();
-	three.attack("Bob");
-	three.attack("target");
-	three.beRepaired(5);
-	three.attack("target");
-	three.takeDamage(100);
-	three.takeDamage(5);
-	three.takeDamage(5);
 	return (0);
 }
