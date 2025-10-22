08f1d86e03edfc0f6472b0d43cd50177bb52a90d
robertrinh <robert.trinh@yahoo.com>
2025-10-22 18:29:58 +0200

    Ford-Johnson finished, should make a test file

---
+++ Rank5/CPP_Modules_5-9/cpp09/ex02/main.cpp
@@ -0,0 +1,18 @@
+#include "PMergeMe.hpp"
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Error: Provide at least one positive integer as argument." << std::endl;
+        return EXIT_FAILURE;
+    }
+    try {
+        PmergeMe sorter;
+        sorter.processInput(argv);
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
